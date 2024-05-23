d2f9546037e22e8522d5bc35f0a2f57048279ede
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-23 19:22:43 +0500

    ui updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../asset/logo.png'
+import searchIcon from '../../asset/search_icon.svg'
+
+
 
 const Navbar = () => {
   return (
-    <div className='navbar'></div>
+    <div className='navbar'>
+      <div className="Navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse By Language</li>
+        </ul>
+      </div>
+      <div className="Navbar-right">
+        <img src={searchIcon} alt="" className='icons' />
+        <p>Childern</p>
+        <img src={searchIcon} alt="" className='icons' />
+      </div>
+    </div>
   )
 }
 
