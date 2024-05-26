5c23a1b99d77d3dca6861dbaeef0e872d8b3bf6d
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-26 21:44:12 +0500

    working in css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import './Navbar.css'
 import logo from '../../asset/logo.png'
 import searchIcon from '../../asset/search_icon.svg'
+import bellIcon from '../../asset/bell_icon.svg'
+import caret from '../../asset/caret_icon.svg'
+import profileIcon from '../../asset/profile_img.png'
+
+
+
 
 
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className="Navbar-left">
+      <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
           <li>Home</li>
@@ -19,11 +25,17 @@ const Navbar = () => {
           <li>Browse By Language</li>
         </ul>
       </div>
-      <div className="Navbar-right">
+      <div className="navbar-right">
         <img src={searchIcon} alt="" className='icons' />
-        <p>Childern </p>
-
-        <img src={searchIcon} alt="" className='icons' />
+        <p>Childern</p>
+        <img src={bellIcon} alt="" className='icons' />
+        <div className="navbar-profile">
+        <img src={profileIcon} alt="" className='profile' />
+        <img src={caret} alt=""/>
+        <div className="dropdown">
+          <p>Sign Out Of Netflix  </p>
+        </div>
+        </div>
       </div>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import heroBanner from '../../asset/hero_banner.jpg'
+import heroTittle from '../../asset/hero_title.png'
+
+
 
 const Home = () => {
   return (
     <div className='home'>
       <Navbar/>
+      <div className="hero">
+        <img src={heroBanner} alt="" className='banner-img' />
+        <div className="hero-caption">
+          <img src={heroTittle} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
