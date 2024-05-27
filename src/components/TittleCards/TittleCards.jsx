b65dc30dee4a0a17fc721dbbcb4380a3fcdb26fe
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-27 19:02:40 +0500

    scroll ui done

--- src/components/TittleCards/TittleCards.jsx
+++ src/components/TittleCards/TittleCards.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './TittleCards.css'
+import cards_data from "../../asset/cards/Cards_data"
 
 const TittleCards = () => {
   return (
-    <div>TittleCards</div>
+    <div className='titlecards'>
+      <h2>Popular On Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        })}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,13 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
 import heroBanner from '../../asset/hero_banner.jpg'
 import heroTittle from '../../asset/hero_title.png'
+import playIcon from '../../asset/play_icon.png'
+import infoIcon from '../../asset/info_icon.png'
+import TittleCards from '../../components/TittleCards/TittleCards'
+
+
+
+
 
 
 
@@ -13,7 +20,12 @@ const Home = () => {
       < div className="hero">
         <img src={heroBanner} alt="" className='banner-img' />
          <div className="hero-caption">
-          <img src={heroTittle} alt="" />
+          <img src={heroTittle} alt="" className='caption-img' />
+          <div className="hero-btns">
+            <button className='btn'><img src={playIcon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={infoIcon} alt="" />More Info</button>
+          </div>
+          <TittleCards/>
         </div>
       </div>
     </div>
