3069be867e347e268d0c0b09e189c0d599b517dd
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-28 22:59:31 +0500

    work on player

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Player.css'
+import backArrow from "../../asset/back_arrow_icon.png"
 
 const Player = () => {
   return (
-    <div className='player'></div>
+    <div className='player'>
+      <img src={backArrow} alt="" />
+      <iframe width="90%" height="90%" src='{https://www.youtube.com/embed/hkHHwA-vEyQ}'
+       title='trailer' frameBorder="0" allowFullScreen></iframe>
+       <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+       </div>
+    </div>
   )
 }
 
